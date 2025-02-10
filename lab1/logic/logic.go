55cf1c497c866acd6abb4113dea16b56ae631de8
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-02-10 17:29:30 +0100

    lab1 task2 done

--- lab1/logic/logic.go
+++ lab1/logic/logic.go
@@ -6,8 +6,16 @@ func CalculateCoeffWorkDry(w float64) float64 {
 	return 100.0 / (100.0 - w)
 }
 
+func CalculateCoeffDryWork(w float64) float64 {
+	return (100.0 - w) / 100.0
+}
+
 func CalculateCoeffWorkBurn(w, a float64) float64 {
 	return 100.0 / (100.0 - w - a)
+}
+
+func CalculateCoeffBurnWork(w, a float64) float64 {
+	return (100.0 - w - a) / 100.0
 }
 
 func CalculateNewPercentage(coeff float64, elements []float64) []float64 {
@@ -29,4 +37,15 @@ func CalculateDryFuelHeatCombustion(heatCombustion, W, workDryCoeff float64) flo
 
 func CalculateBurnFuelHeatCombustion(heatCombustion, W, workBurnCoeff float64) float64 {
 	return math.Round((heatCombustion + 0.025 * W) * workBurnCoeff*100) / 100
+}
+
+func CalculateWorkFuelPercentage(burnWorkCoeff, dryWorkCoeff, C, H, O, S, A, V float64) []float64 {
+	workCarbon := C * burnWorkCoeff
+	workHydrogen := H * burnWorkCoeff
+	workOxygen := O * burnWorkCoeff
+	workSulfur := S * burnWorkCoeff
+	workAsh := A * dryWorkCoeff
+	workVanadium := V * dryWorkCoeff
+
+	return []float64{workCarbon, workHydrogen, workOxygen, workSulfur, workAsh, workVanadium}
 }
